7729fe170a6f25a9fd2f5b4cd0d710ed4309c88e
klassanov <klassanov@gmail.com>
2018-10-24 20:11:06 +0300

    KLS - Added resources and refactoring

--- EmployeeTracker.Web/Controllers/EmployeeController.cs
+++ EmployeeTracker.Web/Controllers/EmployeeController.cs
@@ -30,15 +30,34 @@ namespace EmployeeTracker.Web.Controllers
         }
 
         [HttpPost]
-        public ViewResult Subscribe(EmployeeArrivalSubscriptionGetRequest subscriptionRequest)
+        public async Task<ViewResult> Subscribe(EmployeeArrivalSubscriptionGetRequest subscriptionRequest)
         {
             if(ModelState.IsValid)
             {
-                TempData[Constants.MESSAGE_SUCCESS_KEY] = "Subscription successful!";
+                string url = CreateUrlString(subscriptionRequest.DateParameter);
+                subscriptionRequest.CallbackUrlParameter = url;
+                HttpResponseMessage response = await GetServiceResponseMessage(url);
+                subscriptionRequest.ResponseStatusCode = (int)response.StatusCode;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    SubscriptionToken subscriptionToken = JsonConvert.DeserializeObject<SubscriptionToken>(content);
+                    tokenHelper.StoreToken(subscriptionToken);
+                    subscriptionRequest.SubscriptionTokenValue = subscriptionToken.Token;
+                    subscriptionRequest.SubscriptionTokenExpiryDate = subscriptionToken.Expires;
+                    TempData[Constants.MESSAGE_SUCCESS_KEY] = Resources.msg_subscription_successful;
+                }
+                else
+                {
+                    TempData[Constants.MESSAGE_ERROR_KEY] = string.Format("{0} Response: {1} {2}",
+                        Resources.msg_subscription_not_successful, response.StatusCode, response.ReasonPhrase);
+                }
+                repository.WriteSubscriptionRequest(subscriptionRequest);                
             }
             else
             {
-                TempData[Constants.MESSAGE_ERROR_KEY] = "Subscription not successful!";
+                TempData[Constants.MESSAGE_ERROR_KEY] = Resources.msg_subscription_not_successful;
             }
             return View();
         }
@@ -82,6 +101,7 @@ namespace EmployeeTracker.Web.Controllers
             repository.WriteArrivalPostRequest(postRequest, arrivals);
         }
 
+        /*
         public async Task<ViewResult> List()
         {
             //string url ="http://localhost:51396/api/clients/subscribe?date=2016-03-10&callback=http://localhost:61051/api/arrivals/Post";
@@ -89,16 +109,10 @@ namespace EmployeeTracker.Web.Controllers
 
             DateTime dateParam = new DateTime(1985, 7, 23);
 
-            string url = string.Format("{0}?{1}={2}&{3}={4}",
-               ConfigurationManager.AppSettings["remoteServiceEndpointUrl"],
-               Constants.DATE,
-               dateParam.Date.ToString(ConfigurationManager.AppSettings["subscriptionRequestDateFormat"]),
-               Constants.CALLBACK,
-               ConfigurationManager.AppSettings["applicationServiceCallbackUrl"]);
+            string url = CreateUrlString(dateParam);
 
             EmployeeArrivalSubscriptionGetRequest subscriptionRequest = new EmployeeArrivalSubscriptionGetRequest();
             subscriptionRequest.CallbackUrlParameter = url;
-
             subscriptionRequest.DateParameter = dateParam;
             HttpResponseMessage response = await GetServiceResponseMessage(url);
             subscriptionRequest.ResponseStatusCode = (int)response.StatusCode;
@@ -118,6 +132,7 @@ namespace EmployeeTracker.Web.Controllers
             repository.WriteSubscriptionRequest(subscriptionRequest);
             return View();
         }
+        */
 
         private async Task<HttpResponseMessage> GetServiceResponseMessage(string url)
         {
@@ -127,6 +142,15 @@ namespace EmployeeTracker.Web.Controllers
             return response;
         }
 
+        private string CreateUrlString(DateTime dateParam)
+        {
+            return string.Format("{0}?{1}={2}&{3}={4}",
+              ConfigurationManager.AppSettings["remoteServiceEndpointUrl"],
+              Constants.DATE,
+              dateParam.Date.ToString(ConfigurationManager.AppSettings["subscriptionRequestDateFormat"]),
+              Constants.CALLBACK,
+              ConfigurationManager.AppSettings["applicationServiceCallbackUrl"]);
+        }
 
 
     }
