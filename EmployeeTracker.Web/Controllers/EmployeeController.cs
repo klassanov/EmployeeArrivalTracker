58b05585e12025518cbb08e46b5e805f41fa92ef
klassanov <klassanov@gmail.com>
2018-10-25 11:23:04 +0300

    KLS - Refactoring + comments

--- EmployeeTracker.Web/Controllers/EmployeeController.cs
+++ EmployeeTracker.Web/Controllers/EmployeeController.cs
@@ -18,23 +18,39 @@ namespace EmployeeTracker.Web.Controllers
         private IArrivalsRepository repository;
         private ITokenHelper tokenHelper;
 
+        /// <summary>
+        /// Constructor with automatically injected parameters
+        /// </summary>
+        /// <param name="repository"></param>
+        /// <param name="tokenHelper"></param>
         public EmployeeController(IArrivalsRepository repository, ITokenHelper tokenHelper)
         {
             this.repository = repository;
             this.tokenHelper = tokenHelper;
         }
 
+        /// <summary>
+        /// Employee/Subscribe gets the subscription view for making a new subscription
+        /// </summary>
+        /// <returns></returns>
         public ViewResult Subscribe()
         {
             return View();
         }
 
+        /// <summary>
+        /// POST Emplyee/Subscribe
+        /// <para>Makes a subscripton request to the remote web service with the DateParameter property specified in subscriptionRequest</para>
+        /// <para>Returns the subscription view with a feedback message for the user</para>
+        /// </summary>
+        /// <param name="subscriptionRequest"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<ViewResult> Subscribe(EmployeeArrivalSubscriptionGetRequest subscriptionRequest)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                string url = CreateUrlString(subscriptionRequest.DateParameter);
+                string url = StringHelper.CreateWebSericeSubscriptionRequestUrl(subscriptionRequest.DateParameter);
                 subscriptionRequest.CallbackUrlParameter = url;
                 HttpResponseMessage response = await GetServiceResponseMessage(url);
                 subscriptionRequest.ResponseStatusCode = (int)response.StatusCode;
@@ -53,7 +69,7 @@ namespace EmployeeTracker.Web.Controllers
                     ViewData[Constants.MESSAGE_ERROR_KEY] = string.Format("{0} Response: {1} {2}",
                         Resources.msg_subscription_not_successful, response.StatusCode, response.ReasonPhrase);
                 }
-                repository.WriteSubscriptionRequest(subscriptionRequest);                
+                repository.WriteSubscriptionRequest(subscriptionRequest);
             }
             else
             {
@@ -62,30 +78,52 @@ namespace EmployeeTracker.Web.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Employee/Arrivals gets the employee arrivals view populated with all the entries
+        /// </summary>
+        /// <returns></returns>
         public ViewResult Arrivals()
         {
             IEnumerable<EmployeeArrival> arrivalsList = repository.GetAll<EmployeeArrival>();
             return View(arrivalsList);
         }
 
+        /// <summary>
+        /// Employee/ArrivalsById Gets the employees arrivals view filtered by employeeId
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <returns></returns>
         public ViewResult ArrivalsById(int employeeId)
         {
-            IEnumerable<EmployeeArrival> arrivalsList = repository.GetAll<EmployeeArrival>(x=>x.EmployeeId==employeeId);
-            return View("Arrivals",arrivalsList);
+            IEnumerable<EmployeeArrival> arrivalsList = repository.GetAll<EmployeeArrival>(x => x.EmployeeId == employeeId);
+            return View("Arrivals", arrivalsList);
         }
 
+        /// <summary>
+        /// Employee/PostRequests gets the view with all the post requests that have been received 
+        /// </summary>
+        /// <returns></returns>
         public ViewResult PostRequests()
         {
             IEnumerable<EmployeeArrivalPostRequest> postRequests = repository.GetAll<EmployeeArrivalPostRequest>();
             return View(postRequests);
         }
 
+        /// <summary>
+        /// Employee/SubscriptionsHistory gets the view with all the subscriptions that have been made
+        /// </summary>
+        /// <returns></returns>
         public ViewResult SubscriptionsHistory()
         {
-            IEnumerable<EmployeeArrivalSubscriptionGetRequest> subscriptions=repository.GetAll<EmployeeArrivalSubscriptionGetRequest>();
+            IEnumerable<EmployeeArrivalSubscriptionGetRequest> subscriptions = repository.GetAll<EmployeeArrivalSubscriptionGetRequest>();
             return View(subscriptions);
         }
 
+        /// <summary>
+        /// POST Employee/Post the web service callback url. 
+        /// <para>Validates the received token and writes to the DB the information in the POST request</para>
+        /// </summary>
+        /// <param name="arrivals"></param>
         [HttpPost]
         public void Post(IEnumerable<EmployeeArrival> arrivals)
         {
@@ -103,29 +141,20 @@ namespace EmployeeTracker.Web.Controllers
             {
                 arrivals = null;
             }
-
             repository.WriteArrivalPostRequest(postRequest, arrivals);
         }
 
+        /// <summary>
+        /// Makes the request to url and returns the response
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         private async Task<HttpResponseMessage> GetServiceResponseMessage(string url)
         {
             string requestHeader = ConfigurationManager.AppSettings["ServiceRequestHeader"];
             string requestHeaderValue = ConfigurationManager.AppSettings["ServiceRequestHeaderValue"];
             HttpResponseMessage response = await url.WithHeader(requestHeader, requestHeaderValue).GetAsync();
-            //HttpResponseMessage response = await url.GetAsync();
             return response;
         }
-
-        private string CreateUrlString(DateTime dateParam)
-        {
-            return string.Format("{0}?{1}={2}&{3}={4}",
-              ConfigurationManager.AppSettings["remoteServiceEndpointUrl"],
-              Constants.DATE,
-              dateParam.Date.ToString(ConfigurationManager.AppSettings["subscriptionRequestDateFormat"]),
-              Constants.CALLBACK,
-              ConfigurationManager.AppSettings["applicationServiceCallbackUrl"]);
-        }
-
-
     }
 }
---
+++ EmployeeTracker.Web/Helpers/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeTracker.Domain;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeTracker.Web.Helpers
+{
+    public static class StringHelper
+    {
+        public static string CreateWebSericeSubscriptionRequestUrl(DateTime dateParam)
+        {
+            return string.Format("{0}?{1}={2}&{3}={4}",
+              ConfigurationManager.AppSettings["remoteServiceEndpointUrl"],
+              Constants.DATE,
+              dateParam.Date.ToString(ConfigurationManager.AppSettings["subscriptionRequestDateFormat"]),
+              Constants.CALLBACK,
+              ConfigurationManager.AppSettings["applicationServiceCallbackUrl"]);            
+        }
+
+    }
+}
