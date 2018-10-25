6439259ce82d9d129a0026617150ad95b55c9ccb
klassanov <klassanov@gmail.com>
2018-10-25 12:17:09 +0300

    KLS - Added tests and 2 new solution folders

---
+++ EmployeeTracker.Tests/EmployeeTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeeTracker.Tests
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        [TestMethod]
+        public void TestOfTheTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ EmployeeTracker.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeTracker.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeTracker.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7d5dc78-13de-4404-85a7-7927ce4df01c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
